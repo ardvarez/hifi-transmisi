49470c9e59c792ed1d76fd0b45200be4a8098496
Ardan <48593701+ardvarez@users.noreply.github.com>
2026-08-04 11:42:07 +0700

    chore: update dependencies and regenerate node_modules folder structure

--- py fix/check.py
+++
@@ -1,6 +0,0 @@
-import glob
-files = glob.glob(r'c:\KERJAAN\Project\hifi-ers\hifi-ers\Mutasi\*.html')
-for f in files:
-    content = open(f, encoding='utf-8').read()
-    count = content.count("<style")
-    print(f"{f.split('\\')[-1]}: {count}")
